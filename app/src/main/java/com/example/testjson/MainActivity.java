20a33c2a87bd0b62657485554a9007e67884d20e
wolfblack17 <tuongbui829@gmail.com>
2022-10-12 15:56:25 +0700

    bai tap thay Huy

---
+++ app/src/main/java/com/example/testjson/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.testjson;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView mTextViewResult;
+    private RequestQueue mQueue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextViewResult = findViewById(R.id.text_view_result);
+        Button buttonParse = findViewById(R.id.button_parse);
+
+        mQueue = Volley.newRequestQueue(this);
+        buttonParse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                jsonParse();
+            }
+        });
+    }
+
+    private void jsonParse(){
+        String url ="https://api.jsonserve.com/_tJl4y";
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null
+                , new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONArray jsonArray = response.getJSONArray("student");
+
+                    for (int i = 0; i< jsonArray.length(); i++){
+                        JSONObject student = jsonArray.getJSONObject(i);
+                        String studentid = student.getString("student_id");
+                        String name = student.getString("name");
+                        String email = student.getString("email");
+                        String Class = student.getString("class");
+                        int grade = student.getInt("grade");
+
+                        mTextViewResult.append(studentid+ ","+name+","+","+email+","+Class+","+String.valueOf(grade)+"\n\n");
+
+
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        mQueue.add(request);
+    }
+}
